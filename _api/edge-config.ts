1f26f17daa49980dd2b098e96f64021508c3b2a6
Brendan Allan <brendonovich@outlook.com>
2023-05-15 03:35:05 -0700

    Update edge-config.ts

--- _api/edge-config.ts
+++ _api/edge-config.ts
@@ -1,6 +1,6 @@
 import { get } from "@vercel/edge-config";
 
-export async function GET() {
+export async function() {
   const data = await get("config");
 
   return new Response(JSON.stringify(data));
